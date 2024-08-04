a3e29bde5262af0c8f26ce883607f11bb1064df3
pedresp <pedro.espinosa@alumnos.upm.es>
2024-08-04 21:11:23 +0200

    added main loop

--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
+#include "Obstacle.h"
 #include <ncurses.h>
 
+#include <memory>
+#include <vector>
+
+#include <unistd.h>
+
 int main() {
-    initscr();               
-    printw("Welcome to Terminal Jump"); 
-    refresh();               
-    getch();                 
-    endwin();                
+    initscr();
+    start_color();
+    curs_set(false);
+    printw("Welcome to Terminal Jump");
+
+    init_pair(10, COLOR_WHITE, COLOR_GREEN);
+
+    refresh();
+    getch();
+
+    std::vector<std::unique_ptr<Obstacle>> scenario;
+    scenario.push_back(std::make_unique<Box>(LINES - 1, COLS));
+    scenario.push_back(std::make_unique<Box>(LINES - 1, COLS + 1));
+    scenario.push_back(std::make_unique<Box>(LINES - 1, COLS + 3));
+    scenario.push_back(std::make_unique<Box>(LINES - 1, COLS + 6));
+
+    int finger = 0, scenario_size = scenario.size();
+    while (finger < scenario_size) {
+        for (int i = finger; i < scenario_size; i++)
+            if (!scenario[i]->advance_1_step())
+                finger++;
+
+        refresh();
+        usleep(50 * 1000);
+    }
+
+    getch();
+    endwin();
 
     return 0;
 }
