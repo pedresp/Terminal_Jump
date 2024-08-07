b2d21cd10787c226f9a18e726edfed7b4c5b1267
pedresp <pedro.espinosa@alumnos.upm.es>
2024-08-07 19:37:42 +0200

    header for scenarios' Parser

---
+++ src/Parser.h
@@ -0,0 +1,21 @@
+#ifndef __Parser__
+#define __Parser__
+
+#include <vector>
+#include <istream>
+#include <memory>
+
+#include "Obstacle.h"
+
+enum scenario_states{
+    CORRECT_SCENARIO = 0,
+    UNEXPECTED_CHAR = 1,
+    NO_OBSTACLE_SPECIFIED = 2,
+    NO_WIDTH_SPECIFIED = 3,
+    NOT_VALID_HEIGHT = 4,
+    NOT_VALID_WIDTH = 5
+};
+
+scenario_states read_scenario(std::istream&, std::vector<std::unique_ptr<Obstacle>>&);
+
+#endif
